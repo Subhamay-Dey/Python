510dc72614649ae88545b9c635c3055741e5ba4d
Subhamay-Dey <ncdey1966@gmail.com>
2024-08-31 16:25:23 +0530

    Solved two more python pblms - printing multiplication table of a given num and displaying numbers from a list using for loop.

---
+++ 5.py
@@ -0,0 +1,4 @@
+num = int(input("Enter the number of which the multiplication table will be formed: "))
+
+for i in range(1, 11, 1):
+    print(i * num)
---
+++ 6.py
@@ -0,0 +1,5 @@
+list = [2, 4, 6, 8, 10]
+
+for i in range(0, len(list), 1):
+    print(list[i])
+    
