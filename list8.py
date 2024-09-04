32824dd645d1e4ebe2ce226d11b4289c31a90339
Subhamay-Dey <ncdey1966@gmail.com>
2024-09-04 23:15:02 +0530

    Python - Deletion of a item of the list of a specific index-- .pop(index)

---
+++ list8.py
@@ -0,0 +1,9 @@
+world_animals = ["Anaconda", "Platypus", "Red Panda", "Killer Whale", "Snow Leopard", "King Penguin", "Polar Bear", "Lion", "Zebra", "Plains Bison", "Platypus"]
+
+index = 9
+
+print(f"List before deletion: {world_animals}")
+
+world_animals.pop(index)
+
+print(f"List after deletion: {world_animals}")
