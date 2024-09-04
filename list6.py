3f34e3e15dc9c57d7deb6f7a44a2624ecbe1f913
Subhamay-Dey <ncdey1966@gmail.com>
2024-09-04 22:54:50 +0530

    Python - Concatination of two list to a combined list.

---
+++ list6.py
@@ -0,0 +1,6 @@
+world_animals = ["Anaconda", "Platypus", "Red Panda", "Killer Whale", "Snow Leopard", "King Penguin", "Polar Bear", "Lion", "Zebra", "Plains Bison"]
+world_biomes = ["Tropical Rainforest", "Temperate Forest", "Desert", "Arctic Tundra", "Antarctica", "Alpine Tundra", "Marine", "Freshwater", "Grassland", "Temperate"]
+
+combined_list = world_animals + world_biomes
+
+print(f"The concatinated list is {combined_list}")
