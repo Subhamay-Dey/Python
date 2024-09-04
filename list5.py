2fd63a6f3ae26543d1fcfcb44b35cc2f255799c4
Subhamay-Dey <ncdey1966@gmail.com>
2024-09-04 20:02:31 +0530

    Python - reversing the list items -- .reverse()

---
+++ list5.py
@@ -0,0 +1,5 @@
+world_biomes = ["Tropical Rainforest", "Temperate Forest", "Desert", "Arctic Tundra", "Antarctica", "Alpine Tundra", "Marine", "Freshwater", "Grassland", "Temperate"]
+
+world_biomes.reverse()
+
+print(f"The reverse of the list is: {world_biomes}")
