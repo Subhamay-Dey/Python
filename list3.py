50636f412935936d92c0ab767d8b00bb0f860937
Subhamay-Dey <ncdey1966@gmail.com>
2024-09-04 19:50:21 +0530

    Python - finding index of a string in a list -- .index()

---
+++ list3.py
@@ -0,0 +1,6 @@
+world_animals = ["Anaconda", "Platypus", "Red Panda", "Killer Whale", "Snow Leopard", "King Penguin", "Polar Bear", "Lion", "Zebra", "Plains Bison"]
+
+item_to_find = "Snow Leopard"
+index = world_animals.index(item_to_find)
+
+print(f"The index of the first occurrence of '{item_to_find}' is: {index}")
