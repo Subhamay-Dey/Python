1b38b3415c461c6f929b12294cd12587f11f13d7
Subhamay-Dey <ncdey1966@gmail.com>
2024-09-07 23:44:42 +0530

    Python - Remove Item by Index.

---
+++ list10.py
@@ -0,0 +1,8 @@
+# Given list
+world_animals = ["Anaconda", "Platypus", "Red Panda", "Killer Whale", "Snow Leopard", "King Penguin", "Polar Bear", "Lion", "Zebra", "Plains Bison"]
+
+# Remove item at a specific index
+index_to_remove = 3
+removed_item = world_animals.pop(index_to_remove)
+
+print(f"List after removing item at index {index_to_remove} ('{removed_item}'):", world_animals)
