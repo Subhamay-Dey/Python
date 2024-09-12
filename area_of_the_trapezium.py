80c12f360523c52a914d50073afd4b3a1102318f
Subhamay-Dey <ncdey1966@gmail.com>
2024-09-12 15:38:58 +0530

    Python - area of the trapezium.

---
+++ area_of_the_trapezium.py
@@ -0,0 +1,6 @@
+import math
+side_a=float(input("Enter the length of the first parallel side:"))
+side_b=float(input("Enter the length of the second parallel side:"))
+h=float(input("Enter the height of the trapezium:"))
+ar=0.5*(side_a+side_b)*h
+print(f"Area of the trapezium:{ar}")
