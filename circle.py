c96a82be323247f1def77462a212b5bf7bbb9480
Subhamay-Dey <ncdey1966@gmail.com>
2024-09-12 16:15:12 +0530

    Python - to find area and circumference of the circle.

---
+++ circle.py
@@ -0,0 +1,7 @@
+import math
+radius=float(input("Enter the radius of the circle:"))
+PI=22/7
+ar=PI*radius**2
+cir=2*PI*radius
+print(f"Area of the circle:{ar}")
+print(f"Circumference of the circle:{cir}")
